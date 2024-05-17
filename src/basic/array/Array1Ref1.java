c98fa446815a473baef9a79f24e138cd92de5896
seeyoungryu <seeyoungryu@gmail.com>
2024-05-17 21:09:32 +0900

    update - array

--- src/basic/array/Array1Ref1.java
+++ src/basic/array/Array1Ref1.java
@@ -1,14 +1,22 @@
 package basic.array;
 
+/*
+배열 생성 - new int[5]; ---> int 형 변수 5개를 *연속으로* 만드는 것 ----> 배열의 크기만큼 메모리를 확보함
+> 배열 생성 후-자바는 메모리 어딘가에 있는 이 배열에 접근할 수 있는 참조값(주소)를 반환함
+>배열변수인 int[] students 에 생성된 배열의 참조값을 보관함
+ ( 이 참조값을 통해 배열을 참조할 수 있다 ~~ >> 참조값을 통해 메모리에 있는 실제 배열에 접근하고 사용할 수 있다
+
+ */
+
 public class Array1Ref1 {
 
     public static void main(String[] args) {
-        int[] students; //배열 변수 선언
+        int[] students; //배열 변수 선언 (변수는 한 번 선언함)
         students = new int[5]; //배열 생성 - 배열 크기 : 5 ~~같은 타입의 변수 묶음
 
 
         //변수 값 대입
-        students[0] = 90;
+        students[0] = 90;  //배열 인덱스는 0 부터 시작함
         students[1] = 80;
         students[2] = 70;
         students[3] = 60;

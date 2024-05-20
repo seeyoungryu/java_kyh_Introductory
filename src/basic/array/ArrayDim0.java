c6d71035b09d4b3103a79fcc0f99b3ee060a3f39
seeyoungryu <seeyoungryu@gmail.com>
2024-05-20 15:10:16 +0900

    update - array

---
+++ src/basic/array/ArrayDim0.java
@@ -0,0 +1,13 @@
+package basic.array;
+
+public class ArrayDim0 {
+    public static void main(String[] args) {
+
+        //2차원 배열 만들기
+        int[][] arr = new int[2][3];
+        arr[0][0] = 1;
+
+        System.out.println(arr[0][0]);
+
+    }
+}
